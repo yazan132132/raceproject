015fdb2dec36ca8b69e8d523a3867ab388b5cfbf
zoz <yazan111.yy@gmail.com>
2020-09-14 10:02:28 +0200

    Add project files.

---
+++ Controller1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Controller1
+{
+    public class Class1
+    {
+    }
+}
---
+++ raceproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace raceproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello W!");
+        }
+    }
+}
